2d479081223bbb2e7b20118d40294ad301a52102
Jonas Gamao <jonty.68@hotmail.com>
2019-03-07 17:39:38 -0500

    Clean up with PEP8

--- Hardware/main.py
+++ Hardware/main.py
@@ -8,9 +8,11 @@ import Adafruit_SSD1306 as SSD1306
 import RPi.GPIO as GPIO
 import Adafruit_GPIO.SPI as SPI
 import smbus
+import struct
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
+
 
 class Display:
     def __init__(self, padding):
@@ -21,12 +23,9 @@ class Display:
         self._bottom = self._height - padding
         self._image = Image.new('1', (self._width, self._height))
         self._draw = ImageDraw.Draw(self._image)
-        self._empty = self._draw.rectangle((0, 0, self._width, self._height), outline=0, fill=0)
+        self._empty = self._draw.rectangle((0, 0, self._width, self._height),
+                                           outline=0, fill=0)
         self._font = ImageFont.load_default()
-
-        self._disp.begin()
-        self._disp.clear()
-        self._disp.display()
 
     def _get_draw(self):
         return self._draw
@@ -37,11 +36,12 @@ class Display:
     def display_off(self):
         self._disp.clear()
         self._disp.display()
-    
+
     def display_refresh(self):
         self._disp.begin()
         self._disp.clear()
         self._disp.display()
+
 
 class StepDisplay(Display):
     def __init__(self, steps, padding):
@@ -49,10 +49,12 @@ class StepDisplay(Display):
         Display.__init__(self, padding)
 
     def start_display_count(self):
-        self._draw.rectangle((0, 0, self._width, self._height), outline=0, fill=0)
+        self._draw.rectangle((0, 0, self._width, self._height),
+                             outline=0, fill=0)
 
         self._draw.text((0, self._top), "Steps", font=self._font, fill=255)
-        self._draw.text((0, self._top + 8), str(self._steps), font = self._font, fill=255)
+        self._draw.text((0, self._top + 8), str(self._steps),
+                        font=self._font, fill=255)
 
         self._disp.image(self._image)
         self._disp.display()
@@ -60,27 +62,28 @@ class StepDisplay(Display):
 
     def update_count(self, count):
         self._steps = count
-    
+
 
 class Accel:
     def __init__(self):
         self._accel_mag = LSM303(hires=False)
         self._accel = self._accel_mag.read()
-        self._accel_x, self._accel_y= self._accel
-        self._accel_z = 0
-    
+        self._accel_x, self._accel_y = self._accel
+        # self._accel_z = 0
+
     def get_x(self):
         return self._accel_x
-    
+
     def get_y(self):
         return self._accel_y
-    
+
     def get_z(self):
         return self._accel_z
 
     def get_data_accel(self):
         self._accel = self._accel_mag.read()
-        self._accel_x, self._accel_y= self._accel
+        self._accel_x, self._accel_y = self._accel
+
 
 class TouchSensor:
     def __init__(self, touch, release):
@@ -101,9 +104,12 @@ if (__name__ == "__main__"):
     num_steps = 0
     update_steps = False
     step_display = False
+    accel_sensitivity = 1
     x = 0
     y = 0
     z = 0
+    new_x = 0
+    new_y = 0
 
     test = False
     test2 = True
@@ -112,53 +118,63 @@ if (__name__ == "__main__"):
 
     while True:
         current_touched = touch.last_touch()
-        if (update_steps == True):
-            accel.get_data_accel()
-            new_x = accel.get_x()
-            new_y = accel.get_y()
-            new_z = accel.get_z()
 
-            if (step_display == True):
+        print(str(y))
+        print(str(x))
+
+        if (step_display is True):
                 step_count.start_display_count()
-            else:
-                step_count.display_off()
+        else:
+            step_count.display_off()
+
+        if (update_steps is True):
+            accel.get_data_accel()
+            new_x = accel.get_x()[0]
+            new_y = accel.get_y()
+            #new_z = accel.get_z()
+
+            #new_x = int(new_x[0])
+            new_y = int(new_y[0])
+            #new_z = int(new_z[0])
 
             if ((x != new_x) or (y != new_y) or (z != new_z)):
+            # if (((x > (new_x + accel_sensitivity)) and
+            #      (x < (new_x - accel_sensitivity))) or
+            #     ((y > (new_y + accel_sensitivity)) and
+            #      (y < (new_y - accel_sensitivity)))):
+                                
                 x = new_x
                 y = new_y
-                z = new_z
-                if (test2 == True):
+
+                print(str(new_x + accel_sensitivity))
+                print(str(new_y + accel_sensitivity))
+                # z = int(new_z[0])
+                if (test2 is True):
                     print("X = {0}, Y = {1}".format(x, y))
                 num_steps += 1
                 step_count.update_count(num_steps)
                 print('{0} steps'.format(num_steps))
-        
-        if (test == True):            
+
+        if (test is True):
             step_count.start_display_count()
             num_steps += 1
             step_count.update_count(num_steps)
             print('{0} steps'.format(num_steps))
-        
+
         for i in range(12):
             pin_bit = 1 << i
 
             if current_touched & pin_bit:
                 print('{0} touched!'.format(i))
                 if (i == 0):
-                    if (step_display == False):
+                    if (step_display is False):
                         step_display = True
                     else:
                         step_display = False
                 if (i == 1):
-                    if (update_steps == False):
+                    if (update_steps is False):
                         update_steps = True
                     else:
                         update_steps = False
 
         time.sleep(0.5)
-    
-
-
-
-
-            
