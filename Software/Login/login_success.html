852084e181c1bd8e8379b8b4ff5133f14242005e
rfmaynard <42976498+rfmaynard@users.noreply.github.com>
2019-03-28 01:40:46 -0400

    new login php working with apache2

---
+++ Software/Login/login_success.html
@@ -0,0 +1,9 @@
+<!DOCTYPE HTML>
+ <html>
+<body>
+
+Welcome <?php echo $_REQUEST["uname"]; ?><br>
+Your email address is: <?php echo $_REQUEST["password"]; ?>
+
+</body>
+</html>
---
+++ Software/Login/tester.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+
+
+
+<html lang="en">  
+<head>
+	<meta charset="UTF-8">	
+
+
+</head>
+<body>
+
+
+
+<form action="welcome.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+<input type="submit">
+</form>
+
+</body>
+</html>
