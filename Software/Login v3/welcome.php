30c1902a0ecf38d43519d7b258424cc47afaa494
rfmaynard <42976498+rfmaynard@users.noreply.github.com>
2019-04-04 15:57:47 -0400

    changes

--- Software/Login v3/welcome.php
+++ Software/Login v3/welcome.php
@@ -10,29 +10,7 @@
 		</style>
 	</head>	
 <body class="loginBody" id="runningMan" background="background2.jpg">	
-<?php
-	
-	$email = $_POST['email'];
-	$password = $_POST['passwd'];
-	
-	$output = shell_exec("sudo bash runEz.sh $email $password &");	
-	
-	$error = shell_exec("cat error.txt");
-	
-	if($error == 1){
-		header("Location: fail.php");
-		exit();
-	}
-	elseif($error == 2){
-		header("Location: error.html");
-		exit();
-	}
-	else
-	{
-		header("Location: success.html");
-	}
 
-?>
  
  		<center><h1 style="color: white;"></h1></center>
 	
